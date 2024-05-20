4ef6f8a0e7ef3fe0da814cc0f664ed2a0265aeb0
NoSlovo <zherebchikov456@gmail.com>
2024-05-20 21:25:59 +0500

    AddMovePipes

--- Assets/Scripts/Bootstrapper/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper/Bootstrapper.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Bootstrapper : MonoBehaviour
+namespace Bootstrapper
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Bootstrapper : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private Pipes _pipes;
     }
 }
